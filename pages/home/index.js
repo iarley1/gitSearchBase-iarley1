1e06010788ca4065bea5f8cba1abc68ae13f7a20
iarley <iarley96@gmail.com>
2022-10-17 22:51:19 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,30 @@
 /* Desenvolva sua lógica aqui...*/
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    fetch(`https://api.github.com/users/${input.value}`)
+    .then(response => {
+        if(response.status === 200) {
+            localStorage.setItem('input', input.value)
+            button.innerHTML = '<div class="spinner"</div>'
+            setTimeout(() => {
+                window.location.assign('./pages/profile/profile.html')
+            }, 2000)
+        }else {
+            alert("Este usuario não existe")
+        }
+    })   
+})
+
+function ativarBotao() {
+    if (input.value) {
+      button.disabled = false
+      return
+    }
+    button.disabled = true
+  }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,61 @@
 /* Desenvolva sua lógica aqui...*/
+const ul = document.querySelector('ul')
+const img = document.querySelector('img')
+const nome = document.querySelector('.nome')
+const bio = document.querySelector('.bio')
+const valueInput = localStorage.getItem('input')
+
+
+
+fetch(`https://api.github.com/users/${valueInput}`)
+.then(response => {
+    if(response.status === 200) {
+        return response.json()
+    }
+    
+})
+.then(response => {
+    img.src = response.avatar_url
+    nome.innerText = response.name
+    bio.innerText = response.bio
+})
+
+function api (value) {
+    fetch(`https://api.github.com/users/${value}/repos`)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        renderizar(response, ul)
+    })
+}
+api(valueInput)
+
+function criarLi (repositorio) {
+    const li = document.createElement('li')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+    const div = document.createElement('div')
+    const botaoRepositorio = document.createElement('button')
+    const botaoDemo = document.createElement('button')
+
+    botaoRepositorio.classList.add('botao_repositoio')
+    botaoRepositorio.innerText = "Repositorio"
+
+    botaoDemo.classList.add('botao_demo')
+    botaoDemo.innerText = "Demo"
+
+    h2.innerText = repositorio.name
+    p.innerText = repositorio.description
+
+    li.append(h2, p, div)
+    div.append(botaoRepositorio, botaoDemo)
+
+    return li
+}
+
+function renderizar (repositorio, ul) {
+    repositorio.forEach(element => {
+        ul.appendChild(criarLi(element))
+    });
+}
+
